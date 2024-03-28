112fcc24d3e1b07e0db8c728c6cba2dc1afda739
Moses Abutu <128855851+mosesabutu@users.noreply.github.com>
2024-03-28 17:46:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+/*const toggle = document.getElementById('toggledark');
+const body  = document. querySelector('body');
+
+toggle.addEventListener('click' , function(){
+  this.classList.toggle('bi-moon') ;
+  
+  if(this.classList.toggle('bi-brightness-high-fill')){
+    body.style.background = "aliceblue"
+    body.style.color  = '#333';
+    body.style.transition = '1s'
+  }else{
+    body.style.background = '#111'
+    body.style.color = 'aliceblue'
+    body.style.transition = '1s'
+  }
+})
+*/
+
+
+
